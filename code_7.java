53ca6a95bf99e00da1faeb59126f72d2d098e0c7
Darshna1949 <gangodadarshna@gmail.com>
2026-07-10 16:18:26 +0530

    code7

--- code_7.java
+++ code_7.java
@@ -1,4 +1,26 @@
 //prime number
+import java.util.*;
+
 public class code_7 {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        boolean isPrime = true;
+        if(N<=1){
+            isPrime = false;
+        }
+        for(int i=2;i*i<=N;i++){
+            if(N%i==0){
+                isPrime = false;
+                break;
+            }    
+        }
+        if(isPrime){
+            System.out.println("Prime Number");
+        } else{
+            System.out.println("Not Prime Number");
+        }
+    }
 }
