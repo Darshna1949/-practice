b8181746d4e506026f1da2d81a4daee10a07258b
Darshna1949 <gangodadarshna@gmail.com>
2026-07-13 22:43:15 +0530

    code16

---
+++ code_16.java
@@ -0,0 +1,20 @@
+//pattern 4
+import java.util.*;
+
+public class code_16 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N =sc.nextInt();
+        sc.close();
+
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=(i-1);j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(N-i+1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
